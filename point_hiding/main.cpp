c53b0615df9cf92cee00c13364e744b533ceb033
박준재 <wnswo1109@naver.com>
2025-03-27 17:49:05 +0900

    정보은닉 연습

--- point_hiding/main.cpp
+++ point_hiding/main.cpp
@@ -3,10 +3,56 @@
 
 using namespace std;
 
+bool Point::InitMembers(int xpos, int ypos) {
+    if (xpos < 0 || ypos <0) {
+        cout <<"벗어난 범위의 값" << endl;
+        return false;
+    }
+    x = xpos;
+    y = ypos;
+    cout << "x : " << x << " y : " << y << endl;
+    return true;
+}
 
+int Point::GetX() const {
+    cout << x << endl;
+    return x;
+}
+int Point::GetY() const {
+    cout << y << endl;
+    return y;
+}
+
+bool Point::SetX(int xpos) {
+     if (xpos < 0 ) {
+         cout << "벗어난 범위 값" <<endl;
+         return false;
+     }
+
+    x = xpos;
+    return true;
+}
+
+bool Point::SetY(int ypos) {
+    if (ypos < 0 ) {
+        cout << "벗어난 범위 값" <<endl;
+        return false;
+    }
+
+    y = ypos;
+    return true;
+}
 
 
 int main() {
+    Point x;
+    x.SetX(10);
+    x.GetX();
+    x.SetY(20);
+    x.GetY();
+
+    x.InitMembers(10,20);
+
 
     return 0;
 
